42ca86a85f41bb4f60ccf4d8e93c1df609fba71d
Nurfatehah7145 <147596928+Nurfatehah7145@users.noreply.github.com>
2024-02-29 12:14:15 +0700

    Add files via upload

---
+++ VR Experiment.py
@@ -0,0 +1,14 @@
+﻿import viz
+import vizfx
+import vizconnect
+
+vizconnect.go('vizconnect_config.py')
+
+env = vizfx.addChild('basketball_vr.osgb')
+
+basketball = env.getChild('basketball')
+shoe = env.getChild('shoe')
+grabbable_objects = [basketball, shoe]
+
+grabber = vizconnect.getRawTool('grabber')
+grabber.setItems(grabbable_objects)
